f9fcb70496b56e8344264967020b78b2cd05ad83
fogfog2 <s-eungju-n@hanmail.met>
2018-09-07 01:31:59 +0900

    first

---
+++ kalmanfilter/cvCircleMove.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using kalmanfilter;
+using OpenCvSharp;
+
+namespace kalmanfilter
+{
+    class cvCircleMove
+    {
+        double x=0;
+        double y=0;
+        double dx=0;
+        double dy=0 ;
+        double radius = 0;
+        double t = 0;
+        double dt = 0.02;
+        Mat img;
+        public cvCircleMove()
+        {
+            kalman km = new kalman(); 
+           
+            init();
+            draw();
+        }
+
+        void init()
+        {
+            img = new Mat(new Size(300,300),MatType.CV_8UC3);
+            radius = 100.0;
+            x = 100.0;
+            y = 100.0;
+            
+        }
+
+        void draw()
+        {
+            while(true)
+            { 
+                x = radius * Math.Cos(t) + 150.0;
+                y = radius * Math.Sin(t) + 150.0;
+
+                Cv2.Circle(img, new Point(x, y), 1, new Scalar(255, 255, 255));
+                Cv2.ImShow("t", img);
+
+                if (Cv2.WaitKey(1) == 27)
+                    break;
+                img.SetTo(0);
+                t = t + dt;
+            }
+        }
+    }
+}
---
+++ kalmanfilter/kalman.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCvSharp;
+
+namespace kalmanfilter
+{
+    class kalman
+    {
+        public kalman()
+        {
+            KalmanFilter KF;
+            Mat TransitionM;
+
+            float[] A = new float[] { 1, 1, 0, 1 };
+            TransitionM = new Mat(new Size(2,2),MatType.CV_32FC1);
+            TransitionM.SetArray(0, 0, A);
+
+            KF = new KalmanFilter(4,2,0);
+
+            TransitionM.CopyTo( KF.TransitionMatrix);
+            KF.MeasurementMatrix.SetIdentity(1);
+            KF.ProcessNoiseCov.SetIdentity(1e-5);
+            KF.MeasurementNoiseCov.SetIdentity(1e-1);
+
+        }
+    }
+}
