e79b6225dad7a50fb1b2aaf7e6559fbca0b46281
SeungJun Hwang <s-eungju-n@hanmail.net>
2018-09-07 13:41:47 +0900

    kalmanfilter draw example

--- kalmanfilter/cvCircleMove.cs
+++ kalmanfilter/cvCircleMove.cs
@@ -4,50 +4,103 @@ using System.Linq;
 using System.Text;
 using kalmanfilter;
 using OpenCvSharp;
-
 namespace kalmanfilter
 {
     class cvCircleMove
     {
-        double x=0;
-        double y=0;
-        double dx=0;
-        double dy=0 ;
+        double x = 0;
+        double y = 0;
+        double dx = 1;
+        double dy = 0;
         double radius = 0;
         double t = 0;
-        double dt = 0.02;
+        double dt = 0.1;
         Mat img;
+        Mat predicted_mat;
+        kalman km = new kalman();
+        List<Point> listpt;
+        List<Point> listpt2;
+
         public cvCircleMove()
         {
-            kalman km = new kalman(); 
-           
             init();
             draw();
+
         }
 
         void init()
         {
-            img = new Mat(new Size(300,300),MatType.CV_8UC3);
+            img = new Mat(new Size(300, 300), MatType.CV_8UC3);
+            predicted_mat = new Mat(new Size(2, 1), MatType.CV_32FC1);
+            listpt = new List<Point>();
+            listpt2 = new List<Point>();
             radius = 100.0;
             x = 100.0;
             y = 100.0;
-            
         }
 
         void draw()
         {
-            while(true)
-            { 
-                x = radius * Math.Cos(t) + 150.0;
+            int i = 0;
+            while (true)
+            {
+                if (x < 10)
+                    dx = 1;
+                if (x > 290)
+                    dx = -1;
+
+                x = x + dx;
+
                 y = radius * Math.Sin(t) + 150.0;
 
-                Cv2.Circle(img, new Point(x, y), 1, new Scalar(255, 255, 255));
+
+                Point pt2 = new Point(x, y);
+
+                if (i % 10 == 3 || i % 10 == 6)
+                {
+
+                    Cv2.Circle(img, new Point(x, y), 1, new Scalar(0, 255, 0));
+                    predicted_mat = km.update((float)x, (float)y);
+
+                    listpt2.Add(pt2);
+                }
+                else
+                    predicted_mat = km.update();
+
+                float a = predicted_mat.Get<float>(0, 0);
+                float b = predicted_mat.Get<float>(1, 0);
+
+                Point pt = new Point(a, b);
+
+                listpt.Add(pt);
+
+
+                if(listpt.Count > 10)
+                {
+                    listpt.RemoveAt(0);
+                }
+                if(listpt2.Count>10)
+                    listpt2.RemoveAt(0);
+
+                for (int k = 1; k < listpt.Count; k++)
+                {
+
+                    Cv2.Line(img, listpt[k], listpt[k-1],new Scalar(255,0,0),1);
+                }
+                for (int k = 1; k < listpt2.Count; k++)
+                {
+                    Cv2.Line(img, listpt2[k], listpt2[k - 1], new Scalar(0, 255, 0), 1);
+                }
+
+                Cv2.Circle(img, new Point(a, b), 1, new Scalar(255, 0, 0));
+                //Cv2.Circle(img, new Point(x, y), 1, new Scalar(0, 255, 0));
                 Cv2.ImShow("t", img);
 
                 if (Cv2.WaitKey(1) == 27)
                     break;
                 img.SetTo(0);
                 t = t + dt;
+                i++;
             }
         }
     }
--- kalmanfilter/kalman.cs
+++ kalmanfilter/kalman.cs
@@ -8,22 +8,73 @@ namespace kalmanfilter
 {
     class kalman
     {
+        KalmanFilter KF;
+        Mat TransitionM;
+        Mat MeasurementM;
+        Mat Measurement;
+        Mat Predited;
+
+        float[] A = new float[] {
+            1, 0, 1, 0,
+            0, 1, 0, 1,
+            0, 0, 1, 0,
+            0, 0, 0, 1};
+
+        float[] H = new float[] {
+            1, 0, 0, 0,
+            0, 1, 0, 0};
+
+        float[] H_no_measurement = new float[] {
+            0, 0, 0, 0,
+            0, 0, 0, 0};
+
         public kalman()
         {
-            KalmanFilter KF;
-            Mat TransitionM;
+            init();
+        }
 
-            float[] A = new float[] { 1, 1, 0, 1 };
-            TransitionM = new Mat(new Size(2,2),MatType.CV_32FC1);
+        public void init()
+        {
+            KF = new KalmanFilter(4, 2, 0);
+
+            TransitionM = new Mat(new Size(4, 4), MatType.CV_32FC1);
             TransitionM.SetArray(0, 0, A);
 
-            KF = new KalmanFilter(4,2,0);
+            MeasurementM = new Mat(new Size(4, 2), MatType.CV_32FC1);
+            MeasurementM.SetArray(0, 0, H);
 
-            TransitionM.CopyTo( KF.TransitionMatrix);
-            KF.MeasurementMatrix.SetIdentity(1);
+            Measurement = new Mat(new Size(1, 2), MatType.CV_32FC1);
+            Predited = new Mat(new Size(2, 1), MatType.CV_32FC1);
+
+            TransitionM.CopyTo(KF.TransitionMatrix);
+            MeasurementM.CopyTo(KF.MeasurementMatrix);
+
+            //KF.MeasurementMatrix.SetIdentity(1);
             KF.ProcessNoiseCov.SetIdentity(1e-5);
             KF.MeasurementNoiseCov.SetIdentity(1e-1);
+            KF.ErrorCovPost.SetIdentity(1);
+        }
 
+        public Mat update(float x, float y)
+        {
+            Measurement.Set(0, x);
+            Measurement.Set(1, y);
+            float g = Measurement.At<float>(0);
+            float e = Measurement.Get<float>(0, 0);
+            float f = Measurement.Get<float>(1, 0);
+
+            //Predited = KF.Predict();
+
+            Predited = KF.Correct(Measurement);
+            return Predited;
+        }
+
+        public Mat update()
+        {
+            Predited = KF.Predict();
+            
+
+            return Predited;
         }
     }
 }
